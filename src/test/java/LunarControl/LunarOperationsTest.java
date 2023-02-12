cea3217d31cc065c8c691d45440b7651d70a8371
jimdeal <jimdeal@hotmail.com>
2023-02-12 17:37:01 +0000

    start on LunarOperations tests - basic :
    add map(s)
    add rover(s)

---
+++ src/test/java/LunarControl/LunarOperationsTest.java
@@ -0,0 +1,29 @@
+package LunarControl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LunarOperationsTest {
+
+    @Test
+    public void addMapToOperationalCentre(){
+
+    }
+
+    @Test
+    public void addMapsToOperationalCentre(){
+
+    }
+
+    @Test
+    public void addRoverToOperationalCentre(){
+
+    }
+
+    @Test
+    public void addRoversToOperationalCentre(){
+
+    }
+
+}
