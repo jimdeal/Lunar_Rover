6f2b1a8da7d8f3481bc6642e8e00deae24715f15
jimdeal <jimdeal@hotmail.com>
2023-02-13 12:52:33 +0000

    add in code for adding maps and retrieving by name and id.

--- src/main/java/LunarControl/LunarOperations.java
+++ src/main/java/LunarControl/LunarOperations.java
@@ -1,4 +1,43 @@
 package LunarControl;
 
+import Maps.Grid;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class LunarOperations {
+
+    ArrayList<Grid> mapsInUse = new ArrayList<Grid>();
+
+    public boolean addMapToMapsInUs(Grid map){
+        return mapsInUse.add(map);
+    }
+    public int getNumberOfMapsInUse(){
+        return mapsInUse.size();
+    }
+
+    public Grid getMapByName(String mapName){
+        Iterator<Grid> iterator = mapsInUse.iterator();
+        while (iterator.hasNext()) {
+            Grid grid = iterator.next();
+            if (grid.getMapName().equals(mapName)) {
+                return grid;
+            }
+        }
+        return null;
+
+    }
+
+    public Grid getMapById(String id){
+        Iterator<Grid> iterator = mapsInUse.iterator();
+        while (iterator.hasNext()) {
+            Grid grid = iterator.next();
+            if (grid.getMapId().equals(id)) {
+                return grid;
+            }
+        }
+        return null;
+
+    }
+
 }
--- src/test/java/LunarControl/LunarOperationsTest.java
+++ src/test/java/LunarControl/LunarOperationsTest.java
@@ -1,5 +1,6 @@
 package LunarControl;
 
+import Maps.Grid;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +9,66 @@ class LunarOperationsTest {
 
     @Test
     public void initialiseMap(){
+        LunarOperations lunarOperations = new LunarOperations();
+        Grid grid = new Grid();
+        CoOrds maxSize = new CoOrds(10,10);
+        grid.initialiseGrid("Home", "2",maxSize);
+
+        assertTrue(lunarOperations.addMapToMapsInUs(grid));
+        assertEquals(1, lunarOperations.getNumberOfMapsInUse());
 
     }
+
     @Test
-    public void initialiseMapFail(){
+    public void getMapByName(){
+        LunarOperations lunarOperations = new LunarOperations();
+        Grid grid = new Grid();
+        CoOrds maxSize = new CoOrds(10,10);
+        grid.initialiseGrid("Home", "1",maxSize);
+        assertTrue(lunarOperations.addMapToMapsInUs(grid));
+
+        Grid grid1 = new Grid();
+        CoOrds maxSize1 = new CoOrds(10,10);
+        grid1.initialiseGrid("North", "2",maxSize1);
+        assertTrue(lunarOperations.addMapToMapsInUs(grid1));
+
+        Grid grid2 = new Grid();
+        CoOrds maxSize2 = new CoOrds(10,10);
+        grid2.initialiseGrid("South", "3",maxSize2);
+        assertTrue(lunarOperations.addMapToMapsInUs(grid2));
+
+        assertEquals(3, lunarOperations.getNumberOfMapsInUse());
+        Grid homeMap = lunarOperations.getMapByName(("Home"));
+        assertEquals(homeMap.getMapName(), "Home");
+        assertEquals(homeMap.getMapId(), "1");
+        CoOrds mapSize = homeMap.getMaxSize();
+        assertEquals(mapSize,maxSize);
+    }
+
+    @Test
+    public void getMapById(){
+        LunarOperations lunarOperations = new LunarOperations();
+        Grid grid = new Grid();
+        CoOrds maxSize = new CoOrds(10,10);
+        grid.initialiseGrid("Home", "1",maxSize);
+        assertTrue(lunarOperations.addMapToMapsInUs(grid));
+
+        Grid grid1 = new Grid();
+        CoOrds maxSize1 = new CoOrds(10,10);
+        grid1.initialiseGrid("North", "2",maxSize1);
+        assertTrue(lunarOperations.addMapToMapsInUs(grid1));
+
+        Grid grid2 = new Grid();
+        CoOrds maxSize2 = new CoOrds(10,10);
+        grid2.initialiseGrid("South", "3",maxSize2);
+        assertTrue(lunarOperations.addMapToMapsInUs(grid2));
+
+        assertEquals(3, lunarOperations.getNumberOfMapsInUse());
+        Grid homeMap = lunarOperations.getMapById(("1"));
+        assertEquals(homeMap.getMapName(), "Home");
+        assertEquals(homeMap.getMapId(), "1");
+        CoOrds mapSize = homeMap.getMaxSize();
+        assertEquals(mapSize,maxSize);
 
     }
 
